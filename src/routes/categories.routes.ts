1d259c571497fe77341870e7896a76cbfee80719
Tiago Jose <tiago_jvo@outlook.com>
2022-11-01 06:27:10 -0300

    feat: improve folder structure

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/Categories.repository";
-import { CreateCategoryService } from "../services/CreateCategory.service";
+import { CategoriesRepository } from "../modules/cars/repositories/Categories.repository";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategory.service";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
